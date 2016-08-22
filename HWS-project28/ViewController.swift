76fa0f9feb967d1406a0b9d3ec716365067b05ee
Nikema Prophet <prophen@users.noreply.github.com>
2016-08-22 00:02:23 -0700

    Initial Commit

---
+++ HWS-project28/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HWS-project28
+//
+//  Created by Nikema Prophet on 8/22/16.
+//  Copyright © 2016 Nikema Prophet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
